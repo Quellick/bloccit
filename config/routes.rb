33b80b87cbcdde2a45f6a6fdf94e8e3452bd52d0
Tucker Platt <tucker@bendcable.com>
2017-11-21 22:37:42 +0000

    unkown keyword: id for AdvertisementsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,8 @@
 Rails.application.routes.draw do
-  # get 'posts/index'
-
-  # get 'posts/show'
-
-  # get 'posts/new'
-
-  # get 'posts/edit'
-  
-  # #1 we call the resources method and pass it a Symbol. This instructs Rails to create post routes for creating, updating, viewing, and deleting instances of Post. We'll review the precise URIs created in a moment.
+   resources :advertisements
    resources :posts
-
-  # #2 we remove get "welcome/index" because we've declared the index view as the root view. We also modify the about route to allow users to visit /about, rather than  /welcome/about.
-  # get 'welcome/index'
-  # get 'welcome/about'
   
    get 'about' => 'welcome#about'
 
    root 'welcome#index'
-  
 end
